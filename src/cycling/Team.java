5d01b023a9c53d768eb0b1cb20a77b9d69f2b022
Marcus Carter <marcuscarterdev@gmail.com>
2024-03-17 21:04:47 +0000

    added id attribute

--- src/cycling/Team.java
+++ src/cycling/Team.java
@@ -3,28 +3,26 @@ package cycling;
 import java.io.Serializable;
 
 public class Team implements Serializable {
+    private final int id;
     private final String name;
     private final String description;
-    private final int id;
 
-    public Team(String name, String description) {
+    public Team(int id, String name, String description) {
+        this.id = id;
         this.name = name;
         this.description = description;
     }
-
     public String toString() {
-        return "Team[name="+name+",description="+description+"]";
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
+        return "Team[id="+id+", name="+name+", description="+description+"]";
     }
 
     public int getId() {
         return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
     }
 }
